dbd3b6359c79b050e37d220944b293da066b2d30
Victor Perez <vjperez@mix.wvu.edu>
2019-08-21 14:17:32 -0400

    Updates

---
+++ precache-manifest.6b21da917f4c20669ad90cb1806cf0c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27e4bea8fe4413511a36b5b5bee8ea6",
+    "url": "/wireless/index.html"
+  },
+  {
+    "revision": "8267ff8d0f6a61de3604",
+    "url": "/wireless/static/css/main.00b3f4c8.chunk.css"
+  },
+  {
+    "revision": "f62a37bff8061f1f2035",
+    "url": "/wireless/static/js/2.b13ab0e4.chunk.js"
+  },
+  {
+    "revision": "8267ff8d0f6a61de3604",
+    "url": "/wireless/static/js/main.0170198f.chunk.js"
+  },
+  {
+    "revision": "e9f14d519f9ca5a47749",
+    "url": "/wireless/static/js/runtime~main.d74efc3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wireless/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bcaba2fbbdd86c9f7d2eb887547cef06",
+    "url": "/wireless/static/media/wirelesslogo.bcaba2fb.svg"
+  },
+  {
+    "revision": "0627da78a31a5e1c5e1eb0062e0a67aa",
+    "url": "/wireless/static/media/zine_hq.0627da78.pdf"
+  },
+  {
+    "revision": "ae2823d4ff218cb92a4ed5cdd695fa45",
+    "url": "/wireless/static/media/zine_lq.ae2823d4.pdf"
+  }
+]);
